08fa5f1802f218007da1905bd7d7943273e0262b
Sanghoonio <samp1800@live.com>
2021-02-12 15:17:06 -0500

    basic metadata

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # ibd
+
+Identifying Patterns of Metabolic Dysfunction in the Pathogenesis of IBD
+
+Progress Log:
+
+2/12/21 - sorted out patient data and looked at basic metadata.
+	metadata:
+		protect:
+			20 control
+			206 ulcerative colitis
+				contains histology severity score
+				contains eosinophil grade >= 32
+
+
+		risk:
+			54 control
+			92 crohn's disease
+			42 ulcerative colitis
--- data.R
+++ data.R
@@ -4,19 +4,24 @@ library(GEOquery)
 protect_soft <- getGEO('GSE109142',GSEMatrix=FALSE)
 protect_list <- GSMList(protect_soft)
 protect_seq <- list()
+protect_char <- list()
 
 count = 1
 for (patient in protect_list) {
   protect_seq[[count]] <- read.delim(paste('./data/protect/', list.files('./data/protect')[count], sep=""))
+  protect_char[[count]] <- patient@header[["characteristics_ch1"]]
   count <- count + 1
+  
 }
 
 risk_soft <- getGEO('GSE117993',GSEMatrix=FALSE)
 risk_list <- GSMList(risk_soft)
 risk_seq <- list()
+risk_char <- list()
 
 count = 1
 for (patient in risk_list) {
   risk_seq[[count]] <- read.delim(paste('./data/risk/', list.files('./data/risk')[count], sep=""))
+  risk_char[[count]] <- patient@header[["characteristics_ch1"]]
   count <- count + 1
 }
