dc90bae4e876f9b4973015750fabfbf673faf3b3
Sam Park <samp1800@live.com>
2021-06-02 23:33:26 -0400

    sleuth

--- sleuth.R
+++ sleuth.R
@@ -1,6 +1,7 @@
 library(Biobase)
-library("DESeq2")
-library(ggplot2)
+library("sleuth")
+library(annotables)
+library(tidyverse)
 
 wd <- getwd()
 setwd("./..")
@@ -26,9 +27,18 @@ for (abundance in abundances) {
   gene <- 1
 }
 
-write.csv(tsv_matrix, "tpm_table.csv")
+#write.csv(tsv_matrix, "tpm_table.csv")
 
-#dds <- DESeqDataSetFromMatrix(countData=this,colData=metadata,design=~dex,tidy = TRUE)
+tx2gene <- function(){
+  mart <- biomaRt::useMart(biomart = "ensembl", dataset = "hsapiens_gene_ensembl")
+  t2g <- biomaRt::getBM(attributes = c("ensembl_transcript_id", "ensembl_gene_id",
+                                       "external_gene_name"), mart = mart)
+  t2g <- dplyr::rename(t2g, target_id = ensembl_transcript_id,
+                       ens_gene = ensembl_gene_id, ext_gene = external_gene_name)
+  return(t2g)
+}
+
+t2g <- tx2gene()
 
 setwd(wd)
 
