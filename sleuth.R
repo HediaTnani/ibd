75e19418f2158f5521e46424c6ac44b475ceb5ac
Sam Park <samp1800@live.com>
2021-06-07 23:39:49 -0400

    sleuth

--- sleuth.R
+++ sleuth.R
@@ -24,9 +24,20 @@ datasets <- listDatasets(ensembl)
 mart <- useMart(biomart = "ENSEMBL_MART_ENSEMBL", dataset = "hsapiens_gene_ensembl", host = "ensembl.org")
 ttg <- getBM(attributes = c("ensembl_transcript_id", "transcript_version", "ensembl_gene_id", "external_gene_name", "description", "transcript_biotype"), mart = mart)
 ttg <- rename(ttg, target_id = ensembl_transcript_id, ens_gene = ensembl_gene_id, ext_gene = external_gene_name)
-ttg <- select(ttg, c('target_id', 'ens_gene', 'ext_gene'))
+ttg <- select(ttg, c("target_id", "ens_gene", "ext_gene"))
 
-so <- sleuth_prep(metadata, target_mapping = ttg, aggregation_column = 'ens_gene', extra_bootstrap_summary = TRUE)
+so <- sleuth_prep(metadata, target_mapping = ttg, aggregation_column = "ens_gene", extra_bootstrap_summary = TRUE)
+
+so <- sleuth_fit(so, ~sex, 'reduced')
+
+so <- sleuth_fit(so, ~sex + Diagnosis, 'full')
+
+so <- sleuth_lrt(so, 'reduced', 'full')
+
+sleuth_table_gene <- sleuth_results(so, 'reduced:full', 'lrt', show_all = FALSE)
+sleuth_table_gene <- filter(sleuth_table_gene, qval <= 0.05)
+
+
 
 # tsv_matrix <- matrix(, nrow = 199240, ncol = 226)
 
