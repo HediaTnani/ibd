f29a6e90f9f784a9de6eda486c542a0b9ab1c4be
Sam Park <samp1800@live.com>
2021-07-01 13:39:57 -0400

    gimme script

--- gimme.sh
+++ gimme.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --nodes=28
+#SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --time=7-00:00:00
 #SBATCH --partition=standard
-#SBATCH --mem=131072
+#SBATCH --mem=65536
 #SBATCH --account=tumi
 
 module purge
