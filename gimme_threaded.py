cee581b0f84b287b4ff5adb1e4cbb044708bf026
Sam Park <samp1800@live.com>
2021-07-02 13:28:06 -0400

    gimme threaded

--- gimme_threaded.py
+++ gimme_threaded.py
@@ -2,7 +2,7 @@ import cobra
 import numpy
 import pandas
 
-from pathos.multiprocessing import ProcessingPool as Pool
+from multiprocessing import Pool
 
 from sympy import Min, Max, Add, Mul, Symbol
 from sympy.parsing.ast_parser import parse_expr
@@ -14,82 +14,82 @@ from six import iteritems
 from gimme import gimme_mod
 
 
-# import human genome model
-print('importing human genome model...')
-model = cobra.io.read_sbml_model('data/Human-GEM.xml')
+def gimme_thread(columns):
 
-num_genes = len(model.genes)
-num_samples = 226
+    # import human genome model
+    print('importing human genome model...')
+    model = cobra.io.read_sbml_model('data/Human-GEM.xml')
 
-# import transcript table
-print('importing transcript table...')
-transcript_df = pandas.read_csv('../id_tpm.csv', index_col=0, header=0)
-# transcript_id = pandas.read_csv('../tpm_table.csv', index_col=0, header=0)
+    num_genes = len(model.genes)
+    num_samples = 226
 
-# make sure the index is string, not int, for integration with GIMME
-# print('converting transcript indices...')
-# new_index = ['gene'+str(i) for i in transcript_df.index.tolist()]
-# transcript_df.index = new_index
+    # import transcript table
+    print('importing transcript table...')
+    transcript_df = pandas.read_csv('../id_tpm.csv', index_col=0, header=0)
+    # transcript_id = pandas.read_csv('../tpm_table.csv', index_col=0, header=0)
 
-# read in the metadata which contains sample IDs
-print('importing patient metadata...')
-metadata = pandas.read_csv('data/SraRunTable.txt', index_col=0, header=0)
+    # make sure the index is string, not int, for integration with GIMME
+    # print('converting transcript indices...')
+    # new_index = ['gene'+str(i) for i in transcript_df.index.tolist()]
+    # transcript_df.index = new_index
 
-# transform the simulated counts using the rank-based approach
-print('transforming simulated counts...')
-for sample,subdata in transcript_df.groupby(by=transcript_df.columns,axis=1):
-    # within the sample, sort transcripts by counts
-    transcripts_ranked = subdata[sample].sort_values()
-    # create a vector of ranks from 0 to the length of transcripts
-    rank_vec = [x for x in range(0,len(transcripts_ranked))]
-    rank_vec = pandas.Series(rank_vec)
-    rank_vec.index = transcripts_ranked.index
-    # convert the ranks to a percentile
-    percentiles = (rank_vec + 1)/len(rank_vec)
-    # reorder the percentiles to match the original dataframe
-    percentiles = percentiles.reindex(transcript_df.index)
-    # multiply the original counts by the percentiles
-    transcript_df[sample] = transcript_df[sample].multiply(percentiles)
+    # read in the metadata which contains sample IDs
+    print('importing patient metadata...')
+    metadata = pandas.read_csv('data/SraRunTable.txt', index_col=0, header=0)
 
-# Now normalize each transcript by dividing by the max percentile-normalized
-# value for that transcript across all samples
-print('normalizing transcripts...')
-transcript_df = transcript_df.div(transcript_df.max(axis=1), axis=0)
-transcript_df.fillna(0, inplace=True)
-# transcript_df.reindex(transcript_id.index)
+    # transform the simulated counts using the rank-based approach
+    print('transforming simulated counts...')
+    for sample,subdata in transcript_df.groupby(by=transcript_df.columns,axis=1):
+        # within the sample, sort transcripts by counts
+        transcripts_ranked = subdata[sample].sort_values()
+        # create a vector of ranks from 0 to the length of transcripts
+        rank_vec = [x for x in range(0,len(transcripts_ranked))]
+        rank_vec = pandas.Series(rank_vec)
+        rank_vec.index = transcripts_ranked.index
+        # convert the ranks to a percentile
+        percentiles = (rank_vec + 1)/len(rank_vec)
+        # reorder the percentiles to match the original dataframe
+        percentiles = percentiles.reindex(transcript_df.index)
+        # multiply the original counts by the percentiles
+        transcript_df[sample] = transcript_df[sample].multiply(percentiles)
 
-# print(transcript_df.columns)
+    # Now normalize each transcript by dividing by the max percentile-normalized
+    # value for that transcript across all samples
+    print('normalizing transcripts...')
+    transcript_df = transcript_df.div(transcript_df.max(axis=1), axis=0)
+    transcript_df.fillna(0, inplace=True)
+    # transcript_df.reindex(transcript_id.index)
 
-
-# create the expression profile object using Driven
-print('generating expression profile...')
-exp_prof = ExpressionProfile(identifiers=transcript_df.index.values,
-                             conditions=transcript_df.columns.values,
-                             expression=transcript_df.values)
-
-# print(Symbol(model.genes[1].id))
-# print(exp_prof.to_reaction_dict("SRR6467548", model))
-
-# rename the genes to have a non-numeric character so they don't get evaluated prematurely by driven.
-# This is a problem with the AST parser that cobrapy uses to represent gene identifiers.
-# print('renaming genes...')
-# rename_dict = {gene.id:'gene'+gene.id for gene in model.genes}
-# cobra.manipulation.modify.rename_genes(model,rename_dict)
-# model.repair()
-# # update the GPRs with the 'gene' prefix that we added to the gene IDs
-# for reaction in model.reactions:
-#     old_rule = reaction.gene_reaction_rule
-#     split_by_white = old_rule.split(' ')
-#     new_rule = ''
-#     for entry in split_by_white:
-#         if entry.isdigit():
-#             new_rule += ' gene'+entry
-#         else:
-#             new_rule += ' ' + entry
-#     reaction.gene_reaction_rule = new_rule
+    # print(transcript_df.columns)
 
 
-def gimme_thread(model,transcript_df,metadata,exp_prof,columns):
+    # create the expression profile object using Driven
+    print('generating expression profile...')
+    exp_prof = ExpressionProfile(identifiers=transcript_df.index.values,
+                                 conditions=transcript_df.columns.values,
+                                 expression=transcript_df.values)
+
+    # print(Symbol(model.genes[1].id))
+    # print(exp_prof.to_reaction_dict("SRR6467548", model))
+
+    # rename the genes to have a non-numeric character so they don't get evaluated prematurely by driven.
+    # This is a problem with the AST parser that cobrapy uses to represent gene identifiers.
+    # print('renaming genes...')
+    # rename_dict = {gene.id:'gene'+gene.id for gene in model.genes}
+    # cobra.manipulation.modify.rename_genes(model,rename_dict)
+    # model.repair()
+    # # update the GPRs with the 'gene' prefix that we added to the gene IDs
+    # for reaction in model.reactions:
+    #     old_rule = reaction.gene_reaction_rule
+    #     split_by_white = old_rule.split(' ')
+    #     new_rule = ''
+    #     for entry in split_by_white:
+    #         if entry.isdigit():
+    #             new_rule += ' gene'+entry
+    #         else:
+    #             new_rule += ' ' + entry
+    #     reaction.gene_reaction_rule = new_rule
+
 
     gimme_solutions = {}
 
@@ -110,41 +110,24 @@ def gimme_thread(model,transcript_df,metadata,exp_prof,columns):
             # print(coefficients)
             gimme_solutions[sample] = gimme_solution
 
-cols_1 = numpy.linspace(0,13,14,dtype=int).tolist()
-cols_2 = numpy.linspace(14,27,14,dtype=int).tolist()
-cols_3 = numpy.linspace(28,41,14,dtype=int).tolist()
-cols_4 = numpy.linspace(42,55,14,dtype=int).tolist()
-cols_5 = numpy.linspace(56,69,14,dtype=int).tolist()
-cols_6 = numpy.linspace(70,83,14,dtype=int).tolist()
-cols_7 = numpy.linspace(84,97,14,dtype=int).tolist()
-cols_8 = numpy.linspace(98,111,14,dtype=int).tolist()
-cols_9 = numpy.linspace(112,125,14,dtype=int).tolist()
-cols_10 = numpy.linspace(126,139,14,dtype=int).tolist()
-cols_11 = numpy.linspace(140,153,14,dtype=int).tolist()
-cols_12 = numpy.linspace(154,167,14,dtype=int).tolist()
-cols_13 = numpy.linspace(168,181,14,dtype=int).tolist()
-cols_14 = numpy.linspace(182,195,14,dtype=int).tolist()
-cols_15 = numpy.linspace(196,209,14,dtype=int).tolist()
-cols_16 = numpy.linspace(210,225,14,dtype=int).tolist()
-
 
 data = [
-    (model,transcript_df,metadata,exp_prof,cols_1), 
-    (model,transcript_df,metadata,exp_prof,cols_2),
-    (model,transcript_df,metadata,exp_prof,cols_3),
-    (model,transcript_df,metadata,exp_prof,cols_4),
-    (model,transcript_df,metadata,exp_prof,cols_5),
-    (model,transcript_df,metadata,exp_prof,cols_6),
-    (model,transcript_df,metadata,exp_prof,cols_7),
-    (model,transcript_df,metadata,exp_prof,cols_8),
-    (model,transcript_df,metadata,exp_prof,cols_9),
-    (model,transcript_df,metadata,exp_prof,cols_10),
-    (model,transcript_df,metadata,exp_prof,cols_11),
-    (model,transcript_df,metadata,exp_prof,cols_12),
-    (model,transcript_df,metadata,exp_prof,cols_13),
-    (model,transcript_df,metadata,exp_prof,cols_14),
-    (model,transcript_df,metadata,exp_prof,cols_15),
-    (model,transcript_df,metadata,exp_prof,cols_16)
+    (numpy.linspace(0,13,14,dtype=int).tolist(),), 
+    (numpy.linspace(14,27,14,dtype=int).tolist(),),
+    (numpy.linspace(28,41,14,dtype=int).tolist(),),
+    (numpy.linspace(42,55,14,dtype=int).tolist(),),
+    (numpy.linspace(56,69,14,dtype=int).tolist(),),
+    (numpy.linspace(70,83,14,dtype=int).tolist(),),
+    (numpy.linspace(84,97,14,dtype=int).tolist(),),
+    (numpy.linspace(98,111,14,dtype=int).tolist(),),
+    (numpy.linspace(112,125,14,dtype=int).tolist(),),
+    (numpy.linspace(126,139,14,dtype=int).tolist(),),
+    (numpy.linspace(140,153,14,dtype=int).tolist(),),
+    (numpy.linspace(154,167,14,dtype=int).tolist(),),
+    (numpy.linspace(168,181,14,dtype=int).tolist(),),
+    (numpy.linspace(182,195,14,dtype=int).tolist(),),
+    (numpy.linspace(196,209,14,dtype=int).tolist(),),
+    (numpy.linspace(210,225,14,dtype=int).tolist(),)
     ]
 
 def run_pool():
