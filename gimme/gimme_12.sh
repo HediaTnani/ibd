efa4f7ee4b2586dd3e7e00eda3225ab8feb3dddf
Sam Park <samp1800@live.com>
2021-07-02 14:50:49 -0400

    terrible multiprocessing

---
+++ gimme/gimme_12.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=7-00:00:00
+#SBATCH --partition=standard
+#SBATCH --mem=8192
+#SBATCH --account=tumi
+
+module purge
+module load anaconda/2020.11-py3.8
+
+cd /project/tumi/park/ibd/gimme
+source ../../env/bin/activate
+
+python3 gimme_12.py
--- gimme/gimme_14.sh
+++ gimme/gimme_14.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --ntasks=16
+#SBATCH --ntasks=1
 #SBATCH --time=7-00:00:00
 #SBATCH --partition=standard
-#SBATCH --mem=65536
+#SBATCH --mem=8192
 #SBATCH --account=tumi
 
 module purge
 module load anaconda/2020.11-py3.8
 
-cd /project/tumi/park/ibd
-source ../env/bin/activate
+cd /project/tumi/park/ibd/gimme
+source ../../env/bin/activate
 
-python3 gimme_threaded.py
+python3 gimme_14.py
