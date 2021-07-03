9d0322071f3fe665790d5957ad24048be10337f2
Sam Park <samp1800@live.com>
2021-07-02 21:53:57 -0400

    last time

--- gimme/gimme.sh
+++ gimme/gimme.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
+#SBATCH --ntasks=16
 #SBATCH --time=7-00:00:00
 #SBATCH --partition=standard
-#SBATCH --mem=8192
+#SBATCH --mem=131072
 #SBATCH --account=tumi
 
 module purge
 module load anaconda/2020.11-py3.8
 
-cd /project/tumi/park/ibd
-source ../env/bin/activate
+cd /project/tumi/park/
+source env/bin/activate
+cd ibd/gimme
 
-python3 gimme_1.py
+python3 gimme_threaded.py
